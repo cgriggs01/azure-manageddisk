49571ad7c09ac61480221605e1b38f75c8a480ba
mbpro15-717 <cgriggs01@github.com>
2017-12-07 15:20:43 -0800

    add interpolation

--- config.tf
+++ config.tf
@@ -15,7 +15,7 @@ module "manageddisk" {
     image_reference_id = ""
     import_or_copy_os_type = "Linux"
     managed_disk_name = "test-manageddisk"
-    resource_group_name =  "module-testing"
+    resource_group_name =  "${azurerm_resource_group.module-testing.name}"
     source_resource_id = ""
     source_uri = ""
     storage_account_type = "Standard_LRS"
